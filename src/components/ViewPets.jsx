5845f7657b720351760fd4f6417ae1f76b9b4917
Ad-rian27 <rian270627@gmail.com>
2026-07-10 23:12:43 +0530

    Added ViewPets with static data

--- src/components/ViewPets.jsx
+++ src/components/ViewPets.jsx
@@ -1,9 +1,143 @@
 import React from 'react'
+import NavigationBar from './NavigationBar'
 
 const ViewPets = () => {
-  return (
-    <div>ViewPets</div>
-  )
+    return (
+        <div>
+            <NavigationBar />
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card">
+                                    <img src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
+                                    <div className="card-body">
+                                        <h6 className="card-text">Pet Name: Bruno</h6>
+                                        <h6 className="card-text">Species: Dog</h6>
+                                        <h6 className="card-text">Breed: Labrador Retriever</h6>
+                                        <h6 className="card-text">Age: 2</h6>
+                                        <h6 className="card-text">Gender: Male</h6>
+                                        <h6 className="card-text">Adoption Fee: 2500</h6>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card">
+                                    <img src="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
+                                    <div className="card-body">
+                                        <h6 className="card-text">Pet Name: Misty</h6>
+                                        <h6 className="card-text">Species: Cat</h6>
+                                        <h6 className="card-text">Breed: Indian Shorthair</h6>
+                                        <h6 className="card-text">Age: 1</h6>
+                                        <h6 className="card-text">Gender: Female</h6>
+                                        <h6 className="card-text">Adoption Fee: 1200</h6>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card">
+                                    <img src="https://images.unsplash.com/photo-1517849845537-4d257902454a?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
+                                    <div className="card-body">
+                                        <h6 className="card-text">Pet Name: Rocky</h6>
+                                        <h6 className="card-text">Species: Dog</h6>
+                                        <h6 className="card-text">Breed: Pug</h6>
+                                        <h6 className="card-text">Age: 3</h6>
+                                        <h6 className="card-text">Gender: Male</h6>
+                                        <h6 className="card-text">Adoption Fee: 3000</h6>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card">
+                                    <img src="https://images.unsplash.com/photo-1552053831-71594a27632d?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
+                                    <div className="card-body">
+                                        <h6 className="card-text">Pet Name: Goldie</h6>
+                                        <h6 className="card-text">Species: Dog</h6>
+                                        <h6 className="card-text">Breed: Golden Retriever</h6>
+                                        <h6 className="card-text">Age: 1</h6>
+                                        <h6 className="card-text">Gender: Female</h6>
+                                        <h6 className="card-text">Adoption Fee: 4000</h6>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card">
+                                    <img src="https://images.unsplash.com/photo-1573865526739-10659fec78a5?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
+                                    <div className="card-body">
+                                        <h6 className="card-text">Pet Name: Shadow</h6>
+                                        <h6 className="card-text">Species: Cat</h6>
+                                        <h6 className="card-text">Breed: Bombay Cat</h6>
+                                        <h6 className="card-text">Age: 4</h6>
+                                        <h6 className="card-text">Gender: Male</h6>
+                                        <h6 className="card-text">Adoption Fee: 1500</h6>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card">
+                                    <img src="https://images.unsplash.com/photo-1592194996308-7b43878e84a6?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
+                                    <div className="card-body">
+                                        <h6 className="card-text">Pet Name: Luna</h6>
+                                        <h6 className="card-text">Species: Cat</h6>
+                                        <h6 className="card-text">Breed: Siamese</h6>
+                                        <h6 className="card-text">Age: 2</h6>
+                                        <h6 className="card-text">Gender: Female</h6>
+                                        <h6 className="card-text">Adoption Fee: 2000</h6>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card">
+                                    <img src="https://images.unsplash.com/photo-1585110396000-c9ffd4e4b308?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
+                                    <div className="card-body">
+                                        <h6 className="card-text">Pet Name: Snowy</h6>
+                                        <h6 className="card-text">Species: Rabbit</h6>
+                                        <h6 className="card-text">Breed: White Dwarf</h6>
+                                        <h6 className="card-text">Age: 1</h6>
+                                        <h6 className="card-text">Gender: Female</h6>
+                                        <h6 className="card-text">Adoption Fee: 800</h6>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card">
+                                    <img src="https://images.unsplash.com/photo-1583337130417-3346a1be7dee?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
+                                    <div className="card-body">
+                                        <h6 className="card-text">Pet Name: Simba</h6>
+                                        <h6 className="card-text">Species: Dog</h6>
+                                        <h6 className="card-text">Breed: Indie (Desi)</h6>
+                                        <h6 className="card-text">Age: 2</h6>
+                                        <h6 className="card-text">Gender: Male</h6>
+                                        <h6 className="card-text">Adoption Fee: 500</h6>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default ViewPets
