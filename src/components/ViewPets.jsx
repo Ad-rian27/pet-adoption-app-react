7c42b9e5d20efa473aa964f3f9a66eb77ce565e5
Ad-rian27 <rian270627@gmail.com>
2026-07-10 23:18:45 +0530

    Added JSON properties to ViewPets

--- src/components/ViewPets.jsx
+++ src/components/ViewPets.jsx
@@ -1,7 +1,111 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationBar from './NavigationBar'
 
 const ViewPets = () => {
+
+    const [petdata, changeData] = useState(
+
+        [
+            {
+                "petId": "PET401",
+                "petName": "Bruno",
+                "species": "Dog",
+                "breed": "Labrador Retriever",
+                "age": 2,
+                "gender": "Male",
+                "vaccinated": "Yes",
+                "adoptionFee": "2500",
+                "image": "https://images.unsplash.com/photo-1543466835-00a7907e9de1?fm=jpg&q=60&w=1200&auto=format&fit=crop",
+                "description": "Playful and friendly Labrador, great with kids."
+            },
+            {
+                "petId": "PET402",
+                "petName": "Misty",
+                "species": "Cat",
+                "breed": "Indian Shorthair",
+                "age": 1,
+                "gender": "Female",
+                "vaccinated": "Yes",
+                "adoptionFee": "1200",
+                "image": "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?fm=jpg&q=60&w=1200&auto=format&fit=crop",
+                "description": "Calm indoor cat who loves sunny windowsills."
+            },
+            {
+                "petId": "PET403",
+                "petName": "Rocky",
+                "species": "Dog",
+                "breed": "Pug",
+                "age": 3,
+                "gender": "Male",
+                "vaccinated": "Yes",
+                "adoptionFee": "3000",
+                "image": "https://images.unsplash.com/photo-1517849845537-4d257902454a?fm=jpg&q=60&w=1200&auto=format&fit=crop",
+                "description": "Adorable pug with a big personality, apartment friendly."
+            },
+            {
+                "petId": "PET404",
+                "petName": "Goldie",
+                "species": "Dog",
+                "breed": "Golden Retriever",
+                "age": 1,
+                "gender": "Female",
+                "vaccinated": "No",
+                "adoptionFee": "4000",
+                "image": "https://images.unsplash.com/photo-1552053831-71594a27632d?fm=jpg&q=60&w=1200&auto=format&fit=crop",
+                "description": "Energetic puppy looking for an active family."
+            },
+            {
+                "petId": "PET405",
+                "petName": "Shadow",
+                "species": "Cat",
+                "breed": "Bombay Cat",
+                "age": 4,
+                "gender": "Male",
+                "vaccinated": "Yes",
+                "adoptionFee": "1500",
+                "image": "https://images.unsplash.com/photo-1573865526739-10659fec78a5?fm=jpg&q=60&w=1200&auto=format&fit=crop",
+                "description": "Sleek black cat, independent but affectionate."
+            },
+            {
+                "petId": "PET406",
+                "petName": "Luna",
+                "species": "Cat",
+                "breed": "Siamese",
+                "age": 2,
+                "gender": "Female",
+                "vaccinated": "Yes",
+                "adoptionFee": "2000",
+                "image": "https://images.unsplash.com/photo-1592194996308-7b43878e84a6?fm=jpg&q=60&w=1200&auto=format&fit=crop",
+                "description": "Blue-eyed Siamese who talks back when you talk to her."
+            },
+            {
+                "petId": "PET407",
+                "petName": "Snowy",
+                "species": "Rabbit",
+                "breed": "White Dwarf",
+                "age": 1,
+                "gender": "Female",
+                "vaccinated": "No",
+                "adoptionFee": "800",
+                "image": "https://images.unsplash.com/photo-1585110396000-c9ffd4e4b308?fm=jpg&q=60&w=1200&auto=format&fit=crop",
+                "description": "Gentle dwarf rabbit, perfect first pet for children."
+            },
+            {
+                "petId": "PET408",
+                "petName": "Simba",
+                "species": "Dog",
+                "breed": "Indie (Desi)",
+                "age": 2,
+                "gender": "Male",
+                "vaccinated": "Yes",
+                "adoptionFee": "500",
+                "image": "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?fm=jpg&q=60&w=1200&auto=format&fit=crop",
+                "description": "Rescued street dog, loyal, healthy and house-trained."
+            }
+        ]
+
+    )
+
     return (
         <div>
             <NavigationBar />
@@ -10,133 +114,37 @@ const ViewPets = () => {
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                         <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                {petdata.map(
+                                    (view, index) => {
+                                        return (
+                                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
 
-                                <div className="card">
-                                    <img src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
-                                    <div className="card-body">
-                                        <h6 className="card-text">Pet Name: Bruno</h6>
-                                        <h6 className="card-text">Species: Dog</h6>
-                                        <h6 className="card-text">Breed: Labrador Retriever</h6>
-                                        <h6 className="card-text">Age: 2</h6>
-                                        <h6 className="card-text">Gender: Male</h6>
-                                        <h6 className="card-text">Adoption Fee: 2500</h6>
-                                    </div>
-                                </div>
+                                            <div className="card">
+                                                <img src={view.image} className="card-img-top" height="300px" alt="..." />
+                                                <div className="card-body">
+                                                    <h6 className="card-text">Pet Name: {view.petName}</h6>
+                                                    <h6 className="card-text">Species: {view.species}</h6>
+                                                    <h6 className="card-text">Breed: {view.breed}</h6>
+                                                    <h6 className="card-text">Age: {view.age}</h6>
+                                                    <h6 className="card-text">Gender: {view.gender}</h6>
+                                                    <h6 className="card-text">Adoption Fee: {view.adoptionFee}</h6>
+                                                </div>
+                                            </div>
+                                            </div>
+                                        )
+                                    }
+                                )}
 
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
 
-                                <div className="card">
-                                    <img src="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
-                                    <div className="card-body">
-                                        <h6 className="card-text">Pet Name: Misty</h6>
-                                        <h6 className="card-text">Species: Cat</h6>
-                                        <h6 className="card-text">Breed: Indian Shorthair</h6>
-                                        <h6 className="card-text">Age: 1</h6>
-                                        <h6 className="card-text">Gender: Female</h6>
-                                        <h6 className="card-text">Adoption Fee: 1200</h6>
-                                    </div>
-                                </div>
 
                             </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
 
-                                <div className="card">
-                                    <img src="https://images.unsplash.com/photo-1517849845537-4d257902454a?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
-                                    <div className="card-body">
-                                        <h6 className="card-text">Pet Name: Rocky</h6>
-                                        <h6 className="card-text">Species: Dog</h6>
-                                        <h6 className="card-text">Breed: Pug</h6>
-                                        <h6 className="card-text">Age: 3</h6>
-                                        <h6 className="card-text">Gender: Male</h6>
-                                        <h6 className="card-text">Adoption Fee: 3000</h6>
-                                    </div>
-                                </div>
 
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div className="card">
-                                    <img src="https://images.unsplash.com/photo-1552053831-71594a27632d?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
-                                    <div className="card-body">
-                                        <h6 className="card-text">Pet Name: Goldie</h6>
-                                        <h6 className="card-text">Species: Dog</h6>
-                                        <h6 className="card-text">Breed: Golden Retriever</h6>
-                                        <h6 className="card-text">Age: 1</h6>
-                                        <h6 className="card-text">Gender: Female</h6>
-                                        <h6 className="card-text">Adoption Fee: 4000</h6>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div className="card">
-                                    <img src="https://images.unsplash.com/photo-1573865526739-10659fec78a5?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
-                                    <div className="card-body">
-                                        <h6 className="card-text">Pet Name: Shadow</h6>
-                                        <h6 className="card-text">Species: Cat</h6>
-                                        <h6 className="card-text">Breed: Bombay Cat</h6>
-                                        <h6 className="card-text">Age: 4</h6>
-                                        <h6 className="card-text">Gender: Male</h6>
-                                        <h6 className="card-text">Adoption Fee: 1500</h6>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div className="card">
-                                    <img src="https://images.unsplash.com/photo-1592194996308-7b43878e84a6?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
-                                    <div className="card-body">
-                                        <h6 className="card-text">Pet Name: Luna</h6>
-                                        <h6 className="card-text">Species: Cat</h6>
-                                        <h6 className="card-text">Breed: Siamese</h6>
-                                        <h6 className="card-text">Age: 2</h6>
-                                        <h6 className="card-text">Gender: Female</h6>
-                                        <h6 className="card-text">Adoption Fee: 2000</h6>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div className="card">
-                                    <img src="https://images.unsplash.com/photo-1585110396000-c9ffd4e4b308?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
-                                    <div className="card-body">
-                                        <h6 className="card-text">Pet Name: Snowy</h6>
-                                        <h6 className="card-text">Species: Rabbit</h6>
-                                        <h6 className="card-text">Breed: White Dwarf</h6>
-                                        <h6 className="card-text">Age: 1</h6>
-                                        <h6 className="card-text">Gender: Female</h6>
-                                        <h6 className="card-text">Adoption Fee: 800</h6>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div className="card">
-                                    <img src="https://images.unsplash.com/photo-1583337130417-3346a1be7dee?fm=jpg&q=60&w=1200&auto=format&fit=crop" className="card-img-top" height="300px" alt="..." />
-                                    <div className="card-body">
-                                        <h6 className="card-text">Pet Name: Simba</h6>
-                                        <h6 className="card-text">Species: Dog</h6>
-                                        <h6 className="card-text">Breed: Indie (Desi)</h6>
-                                        <h6 className="card-text">Age: 2</h6>
-                                        <h6 className="card-text">Gender: Male</h6>
-                                        <h6 className="card-text">Adoption Fee: 500</h6>
-                                    </div>
-                                </div>
-
-                            </div>
                         </div>
-
                     </div>
                 </div>
             </div>
 
-        </div>
     )
 }
 
