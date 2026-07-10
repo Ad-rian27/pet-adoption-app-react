0b7f2c32e750df9810cfaef6ba595a9c76f7ca2f
Ad-rian27 <rian270627@gmail.com>
2026-07-10 22:47:47 +0530

    Added AddPet component

---
+++ src/components/AddPet.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+
+const AddPet = () => {
+    return (
+        <div>
+
+            <NavigationBar />
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Pet ID</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Pet Name</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Species</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Breed</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Age</label>
+                                <input type="number" name="" id="" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Gender</label>
+                                <select name="" id="" className="form-control">
+                                    <option value="">Male</option>
+                                    <option value="">Female</option>
+                                </select>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Vaccinated</label>
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="vaccinated" id="yes" value="Yes" />
+                                    <label className="form-check-label" htmlFor="yes">Yes</label>
+                                </div>
+
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="vaccinated" id="no" value="No" />
+                                    <label className="form-check-label" htmlFor="no">No</label>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Adoption Fee</label>
+                                <input type="number" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Image URL</label>
+                                <input type="url" name="" id="" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Description</label>
+                                <textarea name="" id="" className="form-control"></textarea>
+
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                                <button className="btn btn-success">Add Pet</button>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default AddPet
